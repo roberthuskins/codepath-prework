06b4a537c9aff5c1c6be7037f352ddfec4344a8b
Rob <robertchuskins@gmail.com>
2018-09-29 20:55:42 -0700

    initial commit

---
+++ app/src/main/java/edu/csumb/rob/hellorob/MainActivity.java
@@ -0,0 +1,22 @@
+package edu.csumb.rob.hellorob;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((TextView) findViewById(R.id.textView)).setTextColor(R.color.colorPrimary);
+            }
+        });
+    }
+}
